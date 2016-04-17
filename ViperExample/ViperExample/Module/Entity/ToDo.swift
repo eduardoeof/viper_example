6ae12663e1f435b926d9e9163381cbb94d06eadc
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-04-17 11:50:57 -0300

    Create ToDo entity

---
+++ ViperExample/ViperExample/Module/Entity/ToDo.swift
@@ -0,0 +1,19 @@
+//
+//  ToDo.swift
+//  ViperExample
+//
+//  Created by eduardo.ferreira on 4/17/16.
+//  Copyright © 2016 eduardoeof. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    
+    let text: String
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+}
