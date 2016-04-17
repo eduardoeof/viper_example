d51247c451392b85749786dbcc17356005268e5e
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-04-16 21:51:52 -0300

    Create MainProtocols

---
+++ ViperExample/ViperExample/Module/Protocol/MainProtocol.swift
@@ -0,0 +1,30 @@
+//
+//  MainProtocol.swift
+//  ViperExample
+//
+//  Created by eduardo.ferreira on 4/16/16.
+//  Copyright © 2016 eduardoeof. All rights reserved.
+//
+
+import Foundation
+
+protocol MainWireframeProtocol {
+    
+}
+
+protocol MainViewProtocol {
+    
+}
+
+protocol MainPresenterProtocol {
+    
+}
+
+
+protocol MainInteractorInputProtocol {
+    
+}
+
+protocol MainInteractorOutputProtocol {
+    
+}
